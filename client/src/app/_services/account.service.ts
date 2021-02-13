96de060e814c93a27e474dac04fdeafedcd51e73
Jared Schau <deraj320@hotmail.com>
2021-02-12 18:05:13 -0700

    Hooked up login to DB

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http.post(this.baseUrl + 'account/login', model);
+  }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor() {}
+  logginIn: boolean;
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {}
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.logginIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
