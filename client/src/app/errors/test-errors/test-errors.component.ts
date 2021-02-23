bf402f59861b9e633b03b009647fda825531d943
Jared Schau <deraj320@hotmail.com>
2021-02-22 19:39:09 -0700

    handling validation error

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors:string[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -64,6 +65,7 @@ export class TestErrorsComponent implements OnInit {
       },
       (error) => {
         console.log(error);
+        this.validationErrors = error;
       }
     ); 
   }
