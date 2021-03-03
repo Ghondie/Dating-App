4979dfec73e1ebc72e2b80af5e3d13c8a8957e98
Jared Schau <deraj320@hotmail.com>
2021-03-03 16:28:59 -0700

    adding logic to get age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 
@@ -25,5 +26,10 @@ namespace API.Entities
         public string City { get; set; }    
         public string Country { get; set; } 
         public ICollection<Photo>  Photos { get; set; }
+
+        public  int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
